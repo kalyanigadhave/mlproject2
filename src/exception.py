e7275f23a9bdc82ce444cb8355f51bc5609d2453
kalyanigadhave <119089577+kalyanigadhave@users.noreply.github.com>
2023-09-13 08:50:29 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tab=error_detail.exc_info()
@@ -16,6 +16,8 @@ def error_message_detail(error, error_detail:sys):
 
         def __str__(self):
             return self.error_message
+        
+    
         
    
     
